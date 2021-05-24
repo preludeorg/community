2c5dbc84373f6dcb374d7062dbaa851cc1c696e7
Alex Manners <alexandermanners@gmail.com>
2021-05-24 13:33:15 -0400

    update to master

--- ttp_handler.py
+++ ttp_handler.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     results = subprocess.run(['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD'], cwd='./', capture_output=True)
     modified_files = results.stdout.decode('utf-8').rstrip().split('\n')
     for file in modified_files:
-        print(file + '\n')
+        print(file)
         if file.endswith('.yml') and file != '.travis.yml':
             update_ttp(filename=file)
