4a01d20c1876cae31d9a3d75acbe05050e829b46
makk94 <mahina.kaholokula@prelude.org>
2022-05-17 20:52:03 -0400

    dont error and die if missing sliver certs

--- plugins/Sliver/Sliver.js
+++ plugins/Sliver/Sliver.js
@@ -38,7 +38,8 @@ class mTLS extends Listener {
                     };
                 } catch (e) {
                     Events.bus.emit('chat:message', `Missing certificates to launch Sliver mTLS listener! Install to ${this.sliver.certificateDir}.`);
-                    throw new Error(e);
+                    Events.bus.emit('destroy:resource', 'plugin', 'Sliver');
+                    return;
                 }
                 this.listening.mtls = tls.createServer(opts, (socket) => {
                     sockets.add(socket);
